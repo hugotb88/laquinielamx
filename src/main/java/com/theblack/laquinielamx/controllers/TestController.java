8e9171169b363a510481d656282f25cb6e7784f2
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2019-08-24 00:37:00 -0500

    Project structure and test endpoint

---
+++ src/main/java/com/theblack/laquinielamx/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.theblack.laquinielamx.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Victor Hugo Olvera Cruz
+ * @subject Test Controller
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test (){
+        return "Hello Bitches";
+    }
+}
