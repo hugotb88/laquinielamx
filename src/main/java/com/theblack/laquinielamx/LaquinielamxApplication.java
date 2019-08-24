cff53f2b1a7e67403b0b03da4317a7ee22690c18
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2019-08-24 00:22:18 -0500

    First Commit

---
+++ src/main/java/com/theblack/laquinielamx/LaquinielamxApplication.java
@@ -0,0 +1,13 @@
+package com.theblack.laquinielamx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaquinielamxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaquinielamxApplication.class, args);
+    }
+
+}
