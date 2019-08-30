b06f6b6477696a22de24d51fbff1f53ce5423c66
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2019-08-30 15:05:34 -0500

    CQ-5 Get all Matches of the season
    Service added to Match Controller
    Method added to service tha gets a List of Matches using MatchesDao.

--- src/main/java/com/theblack/laquinielamx/services/MatchService.java
+++ src/main/java/com/theblack/laquinielamx/services/MatchService.java
@@ -1,6 +1,11 @@
 package com.theblack.laquinielamx.services;
 
+import com.theblack.laquinielamx.daos.MatchesDao;
+import com.theblack.laquinielamx.models.Match;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Victor Hugo Olvera
@@ -8,4 +13,14 @@ import org.springframework.stereotype.Service;
  * */
 @Service
 public class MatchService {
+
+    @Autowired
+    MatchesDao matchesDao;
+
+    /**
+     * Get all the matches in the Matches Table
+     * */
+    public List<Match> getAllMatchesOfSeason(){
+        return matchesDao.findAll();
+    }
 }
